4748427dd4f49045b3b86660a4efdd5a23799cc9
StNiosem <bauch.aristide@gmail.com>
2024-05-16 19:05:51 +0200

    Moved push library and started database handler

---
+++ src/server/staticPushLibImplementation.js
@@ -0,0 +1,12 @@
+/* STATIC PUSH LIBRARY
+   This might actually work */
+
+import { sleep } from "oAuthTokenImplementation.js";
+
+function PushLibIMPL(pushAddr, pushType, pushContent, pushAuth, pushLang) {
+   webkitURL(pushAddr)
+   payload =  d_PushAddr.push_content(pushContent, pushType)
+
+   pushType = input("pushType input?")
+   passsPushFunctionHandler(input, payload, pushAuth, pushLang)
+}
